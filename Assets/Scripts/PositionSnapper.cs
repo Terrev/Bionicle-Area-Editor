32b536d0402895b40f0c8e91d3b4329e3e4431f3
Terrev <21133460+Terrev@users.noreply.github.com>
2019-07-15 01:30:49 -0700

    bloop bloop

--- Assets/Scripts/PositionSnapper.cs
+++ Assets/Scripts/PositionSnapper.cs
@@ -9,6 +9,7 @@ public class PositionSnapper : MonoBehaviour
 	[System.NonSerialized]
 	public Vector3 loadedPosition = Vector3.zero;
 	
+	// original game raycasts from 2.5 above pickup height to 30 below pickup height, then sets pickup height to 1.5 above point raycast hits
 	public void SnapDownwards(float distanceToSnap, bool warnIfNotPickup)
 	{
 		if
@@ -29,7 +30,7 @@ public class PositionSnapper : MonoBehaviour
 		}
 		
 		RaycastHit hit;
-		if (Physics.Raycast(transform.position, Vector3.down, out hit, distanceToSnap))
+		if (Physics.Raycast(transform.position + new Vector3(0.0f, 2.5f, 0.0f), Vector3.down, out hit, distanceToSnap + 2.5f))
 		{
 			Vector3 newPosition = transform.localPosition + (Vector3.down * hit.distance) + new Vector3(0.0f, 1.5f, 0.0f);
 			float distanceSnapped = newPosition.y - transform.localPosition.y;
