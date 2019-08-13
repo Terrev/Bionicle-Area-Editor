a709a43bde85da0e70eae29b32de2dce17750a15
Terrev <21133460+Terrev@users.noreply.github.com>
2019-08-12 22:18:25 -0700

    unimportant - added vanilla game version folder(s), and a test helper script

---
+++ Assets/Scripts/TestToggler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestToggler : MonoBehaviour
+{
+	// flickers between two gameobjects every time gizmos are drawn (such as every frame while the camera is moving in the editor)
+	// was using to check for visual differences between models
+	
+	public GameObject gameObject1;
+	public GameObject gameObject2;
+	
+	bool blah;
+	
+	void OnDrawGizmos()
+	{
+		if (gameObject1 != null && gameObject2 != null)
+		{
+			gameObject1.SetActive(blah);
+			gameObject2.SetActive(!blah);
+			blah = !blah;
+		}
+	}
+}
